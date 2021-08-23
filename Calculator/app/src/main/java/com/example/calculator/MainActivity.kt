d4fd6eb32314a7a5530a91c1c89810ca5bac3a54
Kemal YANMAZ <ulaskemalyanmaz@gmail.com>
2021-08-24 01:04:46 +0300

    Calculator added new operations '-,/,x'

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.calculator
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,59 +9,92 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var number:String = ""
-        var number1:Int = -1
+        var number1:Double = 0.0
+        var operation = ""
+        var result=""
         button1.setOnClickListener {
             number+="1"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button2.setOnClickListener {
             number+="2"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button3.setOnClickListener {
             number+="3"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button4.setOnClickListener {
             number+="4"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button5.setOnClickListener {
             number+="5"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button6.setOnClickListener {
             number+="6"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button7.setOnClickListener {
             number+="7"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button8.setOnClickListener {
             number+="8"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button9.setOnClickListener {
             number+="9"
-            textView_result.setText(number)
+            textView_result.text = number
         }
         button0.setOnClickListener {
             number+="0"
-            textView_result.setText(number)
+            textView_result.text = number
+        }
+        buttonDot.setOnClickListener {
+            number+="."
+            textView_result.text = number
         }
         buttonClear.setOnClickListener {
-            textView_result.setText("")
+            textView_result.text = ""
             number = ""
-            number1 = 0
+            number1 = 0.0
         }
-        buttonPlus.setOnClickListener {
-            number1 = textView_result.text.toString().toInt()
+        buttonSummation.setOnClickListener {
+            number1 = textView_result.text.toString().toDouble()
             number = ""
+            operation = "+"
+        }
+        buttonDivision.setOnClickListener {
+            number1 = textView_result.text.toString().toDouble()
+            number = ""
+            operation = "/"
+        }
+        buttonMultiplication.setOnClickListener {
+            number1 = textView_result.text.toString().toDouble()
+            number = ""
+            operation = "*"
+        }
+        buttonSubtraction.setOnClickListener {
+            number1 = textView_result.text.toString().toDouble()
+            number = ""
+            operation = "-"
         }
 
         buttonEqual.setOnClickListener {
-            textView_result.setText((number1 + number.toInt()).toString())
+            when(operation){
+                "+"->result=(number1 + number.toDouble()).toString()
+                "-"->result=(number1 - number.toDouble()).toString()
+                "*"->result=(number1 * number.toDouble()).toString()
+                "/"->result=(number1 / number.toDouble()).toString()
+            }
+            val splitResult = result.split(".")
+            if(splitResult.get(1).toInt()==0){
+                    textView_result.text = splitResult.get(0)
+            }else{
+                textView_result.text = result.toDouble().toString()
+            }
         }
     }
 }
